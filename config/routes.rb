f605862eb118ac57f4a1fdd76f42e89f316d7625
Goruchie <marcosgualtero43253@gmail.com>
2023-01-30 20:21:38 -0300

    Add more controller features

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get '/users/:user_id/posts', to: 'posts#index'
-  get '/users/:user_id/posts/:id', to: 'posts#show'
-
   resources :users, only: %i[index show]
   resources :posts, only: %i[index show]
 end
