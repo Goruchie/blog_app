da728b620438c4c071b167843a3bde01fa774d17
Goruchie <marcosgualtero43253@gmail.com>
2023-01-31 21:35:14 -0300

    Fix offences

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET #index' do
-    before(:example) { get '/posts' }  # get(:index)
+    before(:example) { get '/posts' } # get(:index)
 
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
       expect(response).to render_template('index')
     end
-    it "includes correct placeholder text" do
-      expect(response.body).to include("Show posts for a given user:")
+    it 'includes correct placeholder text' do
+      expect(response.body).to include('Show posts for a given user:')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET #index' do
-    before(:example) { get '/users' }  # get(:index)
-    it "is a success" do
+    before(:example) { get '/users' } # get(:index)
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
     it "renders 'index' template" do
       expect(response).to render_template('index')
     end
-    it "includes correct placeholder text" do
-      expect(response.body).to include("Show users:")
+    it 'includes correct placeholder text' do
+      expect(response.body).to include('Show users:')
     end
   end
 end
