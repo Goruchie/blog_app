eb1b79de0cefc8c36a600224b51cbf7ebf2690a5
Goruchie <marcosgualtero43253@gmail.com>
2023-02-04 13:24:24 -0300

    Fix comments creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'users#index'
+
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index show] do
       resources :likes, only: %i[create show]
