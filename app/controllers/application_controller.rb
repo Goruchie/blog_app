5e803ef96a22a0cb54b5248e8bcf1ac43e760be0
Goruchie <marcosgualtero43253@gmail.com>
2023-02-09 21:20:42 -0300

    Fix rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery prepend: true
-    before_action :update_allowed_parameters, if: :devise_controller?
-    before_action :authenticate_user!
-  
-    protected
-  
-    def update_allowed_parameters
-      devise_parameter_sanitizer.permit(:sign_up) do |u|
-        u.permit(:name, :bio, :email, :password, :password_confirmation, :role)
-      end
-      devise_parameter_sanitizer.permit(:account_update) do |u|
-        u.permit(:name, :bio, :email, :password, :current_password, :role)
-      end
+  protect_from_forgery prepend: true
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :email, :password, :password_confirmation, :role)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :email, :password, :current_password, :role)
     end
   end
+end
